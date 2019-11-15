79ff5ce5cfab8e1eaf0b5300cb6c6ffae648c6b9
melpon <shigemasa7watanabe+github@gmail.com>
2019-11-15 12:56:04 +0900

    よく分からなくなったので $ErrorActionPreference = 'Stop' をやめる

--- gabuild.ps1
+++ gabuild.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = 'Stop'
-
 if (Test-Path vswhere.exe) {
     Remove-Item vswhere.exe -Force
 }
@@ -35,7 +33,7 @@ $Env:DEPOT_TOOLS_WIN_TOOLCHAIN = "0"
 $Env:PYTHONIOENCODING = "utf-8"
 
 # depot_tools
-git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 $Env:PATH = "$REPO_DIR\depot_tools;$Env:PATH"
 
 # dbghelp.dll が無いと怒られてしまうので所定の場所にコピーする
